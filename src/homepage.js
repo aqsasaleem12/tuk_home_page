f49b8aa7988a84f771cf13a5156391ba0a704175
Aqsa saleem <aqsasaleem.alphasqua@gmail.com>
2023-06-05 16:14:06 +0500

    tuk_home

--- src/homepage.js
+++ src/homepage.js
@@ -3,10 +3,10 @@ import React from "react";
 const Homepage = () => {
   return (
     <>
-      <div className=" bg-[#030014]">
+      <div className=" ">
         {/* top-bar */}
 
-        <div className="fixed w-full bg-main top-0 colo-gradient py-5 ">
+        <div className="fixed w-full bg-main top-0 colo-gradient py-5 z-30">
           <div className="flex items-center relative  justify-between max-w-7xl w-full mx-auto">
             {/* logo */}
             <div>
@@ -33,12 +33,12 @@ const Homepage = () => {
                 Get started for fee
               </button>
             </div>
-            <div className="bgr-light absolute w-[400px] h-[300px] left-[40%] right-[50%]"></div>
+            <div className="bgr-light absolute w-[400px] h-[200px] left-[40%] right-[50%]"></div>
           </div>
         </div>
 
         {/* main sec */}
-        <div className="main-sec">
+        <div className="main-se ">
           <div className="flex flex-col gap-2 justify-center items-center pt-[200px]">
             <h1 className="text-[64px] font-semi-bold leading-[76px] text-center font-semibold color-gradient">
               Deliver Better with Tailwind CSS
@@ -51,45 +51,64 @@ const Homepage = () => {
               right in your mail box
             </p>
           </div>
+        </div>
 
-          {/*framew0rk-svgs */}
-          <div className="flex flex-col gap-6">
-            <div className="flex justify-center mt-11 gap-8">
+        <div className="relative w-full ">
+          <video
+            className="w-full h-full max-h-[981px] relative "
+            height="100%"
+            width="100%"
+            src="/vedio/animation.mp4"
+            sidedock="0"
+            title="0"
+            frameBorder="0"y
+            // allow="autoplay; picture-in-picture"
+            autoPlay
+            loop
+            muted
+            type="video/mp4"
+            // onEnded={(event) => {
+            //   event.target.play();
+            // }} // Restart the video when it ends
+          ></video>
+
+          <div className=" mx-auto  max-w-[1029px] absolute  inset-0  max-h-[648px]">
+            {/*framew0rk-svgs */}
+            <div className="flex flex-col gap-6 mt-11">
+              <div className="flex justify-center   gap-8">
+                <img
+                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/taulwind.svg"
+                  alt="taulwind"
+                />
+                <img
+                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/angular.svg"
+                  alt="angular"
+                />
+                <img
+                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/react.svg"
+                  alt="react"
+                />
+                <img
+                  src="https://tuk-cdn.s3.amazonaws.com/can-uploader/vue.svg"
+                  alt="vue"
+                />
+              </div>
+              {/* main-button */}
+              <div className="text-center">
+                <button className="max-w-[308px] w-full text-lg  rounded-[7px] py-4 text-white uppercase button leading-[28.8px]">
+                  Get started for free
+                </button>
+              </div>
+            </div>
+            <div className="border border-white/10 !rounded-xl p-1 mt-12">
               <img
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/taulwind.svg"
-                alt="taulwind"
-              />
-              <img
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/angular.svg"
-                alt="angular"
-              />
-              <img
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/react.svg"
-                alt="react"
-              />
-              <img
-                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/vue.svg"
-                alt="vue"
+                src="https://tuk-cdn.s3.amazonaws.com/can-uploader/Exportthi%20inSVG.svg"
+                className="mx-auto rounded-xl"
               />
             </div>
-            {/* main-button */}
-            <div className="text-center">
-              <button className="max-w-[308px] w-full text-lg  rounded-[7px] py-4 text-white uppercase button leading-[28.8px]">
-                Get started for free
-              </button>
-            </div>
-          </div>
-        </div> 
-
-        {/* components-img */}
-        <div className="mt-11 w-full">
-          <div className="border border-white/10 mx-auto !rounded-xl max-w-[1029px]  p-2">
-            <img
-              src="https://tuk-cdn.s3.amazonaws.com/can-uploader/TukHeroImgv3.png"
-              className="mx-auto rounded-xl"
-            />
           </div>
         </div>
+
         {/* industry leaders-heading */}
         <div className="flex flex-col gap-5">
           <div className="mt-[15px]">
